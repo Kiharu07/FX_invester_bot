2fd88ebd634cc3a7c39d6fae090061e0bedc8954
Kiharu07 <sak1001haruki@gmail.com>
2025-08-01 22:23:53 +0900

    為替と時刻のリストをmain()に追加

--- fx_bot.py
+++ fx_bot.py
@@ -2,10 +2,12 @@ import time #時間を扱うため
 import yaml #config.yamlを読み込むため
 import logging
 from logging.handlers import RotatingFileHandler # ログをファイルに保存するため（どういうこと？）
-
 import random #ダミーデータ用設定
+from datetime import datetime #現在の時刻を取得するため
+import csv #CSVファイル(記録用)のため
 
-
+#ログ設定
+open("trade_log.txt", "w", encoding="utf-8").close() #trade_log.txtを空にする（初期化）
 logger = logging.getLogger("fxBotLogger") #"fxBotという名前のログ記録のノートを作成"
 logger.setLevel(logging.INFO) #記録する基準となる重要度をINFO（通常）に設定
 handler = RotatingFileHandler("trade_log.txt",maxBytes=1024*1024,backupCount=2,encoding="utf-8") #trade_log.textファイルに記録し、1MBまで保存でき、２つまで古いファイルを残す。
@@ -13,6 +15,7 @@ formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s') #日�
 handler.setFormatter(formatter) #設定したフォーマットをペン先にセット
 logger.addHandler(handler) #ペン先をノートにセット
 
+#設定読み込み
 with open("config.yaml", "r",encoding="utf-8") as unchi: #config.yamlを読み込んで（r=readの略）、unchiという名前で使えるようにする
     config = yaml.safe_load(unchi) #yamlの内容をconfigという名前でpythonで使える形に変換
 
@@ -21,8 +24,12 @@ sell_border = config["strategy"]["sell_border"] #config.yamlのstrategyのsell_b
 volume = config["strategy"]["volume"] #config.yamlのstrategyのvolumeをvolumeという名前で使えるようにする
 interval = config["interval"] #config.yamlのintervalをintervalという名前で使えるようにする
 
-trademode = "IDLE"#初期トレードモードをIDLE（何もしない）に設定
+#為替と時刻リストのデータボックス
+Price_history = [] #為替情報を記録（この数値をいろいろ使う）
+Time_history = [] #時刻を記録（使わないかも）
+HISTORY_LENGTH = 100 #履歴の長さ(調整用)
 
+#売買判断関数
 def get_buy_point(price):
     return random.uniform(0,100) #ランダムな買いポイントを生成
 
@@ -38,23 +45,29 @@ def should_trade(buy_point,sell_point,buy_border,sell_border,trademode):
             return {"type":"sell","reason":f"SellPoint={sell_point:.2f} > SellBorder={sell_border}"} #売る時のtypeと理由を返す
     return None #トレードしない場合はNoneを返す
 
+#ダミー価格設定
 def current_price():
     return random.uniform(140.0,160.0) #ダミー価格取得の関数を設定
 
+#メイン処理ループ
 def main():
     global trademode #trademodeを関数外でもどこでも使えるようにする
 
     logger.info("FX Bot 起動") #FX Botの起動をtrade_log.txtに記録.(logのレベルはinfoなので、重要度は通常)
+    trademode = "IDLE" #トレードモードをIDLEに設定
+    
     while True: #無限ループ
         try:#エラーが起きても止まらずに続けたい
-            
+            #各種データ取得
             price = current_price() #現在の価格を取得
             buy_point = get_buy_point(price) #買いポイントを取得
             sell_point = get_sell_point(price) #売りポイントを取得
             logger.info(f"[価格]: {price:.2f}|[BuyPoint]: {buy_point:.2f}|[SellPoint]: {sell_point:.2f}|[Mode]{trademode}") #現在の価格と買いポイント、売りポイントをtrade_log.txtに記録
-            
+
+            #取引判断
             decision = None #decisionを初期化
             decision = should_trade(buy_point,sell_point,buy_border,sell_border,trademode) #トレードの判断をする
+            
             if decision: #decisionがNoneでない場合（トレードする場合）
                 logger.info(f"→取引判断:{decision['type']} 実行 | 理由: {decision['reason']}") #decisonの内容をtrade_log.txtに記録
                 if decision["type"] == "buy": #買う場合
@@ -65,9 +78,22 @@ def main():
                 logger.info("→取引無し")
             time.sleep(interval) #設定した時間だけ待つ
 
+            #価格履歴と時刻履歴の一時記憶
+            Price_history.append(price) #価格履歴に現在の価格を追加
+            Time_history.append(datetime.now().strftime("%Y-%m-%d %H:%M:%S")) #時刻履歴に現在の時刻を追加
+            if len(Price_history) > HISTORY_LENGTH: #価格履歴がHISTORY_LENGTHを超えたら
+                del Price_history[0] #価格の最初の要素を削除  
+            if len(Time_history) > HISTORY_LENGTH: #時刻履歴がHISTORY_LENGTHを超えたら
+                del Time_history[0] #時刻の最初の要素を削除
+
+            print(Price_history) #価格履歴を表示（デバッグ用）
+
+
         except Exception as e: #何かエラーが起きたら
             logger.info(f"エラー発生: {e}") #エラー内容をtrade_log.txtに記録
             time.sleep(10)#10秒待ってから再試行
 
 if __name__ == "__main__": #ほかのファイルからインポートされた場合に勝手に実行しないようにする
     main() # main関数を実行
+
+
